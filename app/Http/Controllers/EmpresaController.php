78bd06f7d7269c2724f893bb58bc6429e0be3ce4
VINCULACION-E4 <vinculacionequipo4@gmail.com>
2025-04-02 11:52:44 -0600

    crear bien ofertas de trabajo

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -63,7 +63,7 @@ class EmpresaController extends Controller
                 'area_trabajo' => $data['area_trabajo'],
                 'carrera_solicitada' => $data['carrera'],
                 'usuarios_empleador_idusuarios_empleador' => $user->idusuarios_empleador,
-                'estado' => $data['Pendiente']
+                'estado' => 'Pendiente'
              ]);
         }
         return view ('empresa.completado');
